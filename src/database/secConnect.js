a52d7426bf686a01a2520c500513afcbf7ded8e3
aek20 <66400022+aek20@users.noreply.github.com>
2022-06-08 21:40:28 +0300

    Update secConnect.js

--- src/database/secConnect.js
+++ src/database/secConnect.js
@@ -1,4 +1,4 @@
 // import mongoose from 'mongoose';
-// mongoose.mongoose.createConnection("mongodb+srv://aek123:1234@cluster0.ptuiq.mongodb.net/chatbotb?retryWrites=true&w=majority")
+// mongoose.mongoose.createConnection("")
 // const conn = mongoose.connection
 // export default conn
