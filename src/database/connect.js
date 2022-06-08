9402dff70ae6eec9a2a80298a9eba30f7fcef638
aek20 <66400022+aek20@users.noreply.github.com>
2022-06-08 21:39:53 +0300

    Update connect.js

--- src/database/connect.js
+++ src/database/connect.js
@@ -2,6 +2,6 @@ import mongoose from 'mongoose';
 // import dotenv from 'dotenv';
 // dotenv.config()
 // const databaseUrl = process.env.MONGO_URI
-mongoose.connect("mongodb+srv://aek123:1234@cluster0.ptuiq.mongodb.net/chatbotb?retryWrites=true&w=majority")
+mongoose.connect("")
 const conn = mongoose.connection
 export default conn
